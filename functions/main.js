40ce8923d50e1ae6c136c88d751ca4c133672344
Dmitry Kharlamov <majsberg@yandex.ru>
2019-04-13 00:11:32 +0300

    Вторая задача. Расшифровка данных

--- functions/main.js
+++ functions/main.js
@@ -1,39 +1,33 @@
 "use strict";
 
-let solution = new Object;
-
-function getSolutions(a, b, c) {
-  let d = Math.pow(b, 2) - 4 * a * c;
-  let x1, x2;
-  if (d < 0) {
-    solution.D = d;
-  } else if (d == 0) {
-    x1 = -b / (2 * a);
-    solution.D = d;
-    solution.roots = [x1];
-  } else {
-    x1 = (-b + Math.sqrt(d)) / (2 * a);
-    x2 = (-b - Math.sqrt(d)) / (2 * a);
-    solution.D = d;
-    solution.roots = [x1, x2];
-  }
-  return solution;
+let secretData = {
+  aaa: 0,
+  bbb: 1
 }
 
-function showSolutionsMessage( a, b, c ) {
-  let result = getSolutions (a, b, c);
-  if (c > 0) {
-    console.log(`Вычисляем корни квадратного уравнения ${a}x² + ${b}x + ${c}`);
-  } else {
-    console.log(`Вычисляем корни квадратного уравнения ${a}x² + ${b}x ${c}`);
+function getPersonData() {
+  function realName() {
+    let realNameObj = new Object;
+    for (let key in secretData) {
+      let value = secretData[key];
+      if (key == "aaa") {
+        realNameObj.firstName;
+        if (value == 0) {
+          realNameObj.firstName = "Родриго";
+        } else {
+          realNameObj.firstName = "Эмильо";
+        }
+      } else {
+        realNameObj.lastName;
+        if (value == 0) {
+          realNameObj.lastName = "Родриго";
+        } else {
+          realNameObj.lastName = "Эмильо";
+        }
+      }
+    }
+    return(realNameObj);
   }
-  console.log(`Значение дискриминанта: ${solution.D}`);
-  if (solution.D < 0) {
-    console.log('Уравнение не имеет вещественных корней');
-  } else if (solution.D == 0) {
-    console.log(`Уравнение имеет один корень X₁: ${solution.roots}`);
-  } else {
-    console.log(`Уравнение имеет два корня. X₁ = ${solution.roots[0]}, X₂ = ${solution.roots[1]}`)
-  }
+  console.log(realName(secretData));
 }
-showSolutionsMessage(1, 2, -3)
+getPersonData(secretData);
